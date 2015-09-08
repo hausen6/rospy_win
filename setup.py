019d44d56cf8721395c30928f06ff45c21d2054b
hausen6 <hausen62480@gmail.com>
2015-09-09 00:38:58 +0900

    setup.pyを華麗に修正

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ from setuptools import setup, find_packages
 setup(
         name="rospy",
         author="hausen6",
-        discription="rospy for windows (indigo)",
-        packages=find_packages(),
-        install_requires = ["rospkg>=1.0.35",
-                            "catkin_pkg>=0.2.10",
-            ]
+        description="rospy for windows (indigo)",
+        packages=find_packages("src"),
+        package_dir={"": "src", }
+        # install_requires = ["rospkg>=1.0.35",
+        #                     "catkin_pkg>=0.2.10",
+            # ]
 )
