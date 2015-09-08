dbba0ceb2c56406bbd16154470a05f70f397aefe
hausen6 <hausen62480@gmail.com>
2015-09-08 21:47:15 +0900

    setup.py追加

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+        name="rospy",
+        author="hausen6",
+        discription="rospy for windows (indigo)",
+        packages=find_packages(),
+           # ["catkin", "client", "genmsg", "genpy",
+           #  "roscpp", "rosgraph", "rosgraph_msgs", "roslib",
+           #  "rospy", "std_msgs", "std_srvs"]
+)
